dad75709c1d1acb2ba82833d178483b40e71dec8
Sergi Ibaka <soredavy8@gmail.com>
2026-08-03 13:33:10 +0000

    fix: ajout des imports manquants pour les signals

--- src/app/features/locataires/locataires.component.ts
+++ src/app/features/locataires/locataires.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { NgIconComponent } from '@ng-icons/core';
